be303b9988de9f5d4cd217de1497f4a2b51d310f
gonza <gonzalo.muscolo@gmail.com>
2021-05-23 21:42:53 -0300

    adding config for hotwire-devise and scanfold devise

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,8 @@
 class User < ApplicationRecord
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable
   rolify
 
   after_create :assign_default_role
